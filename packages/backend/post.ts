586327728b51b3f4ba0c622f4a42d7ec3bbbfba3
MatteoMer <matteo.merv@gmail.com>
2023-07-22 22:19:51 +0200

    post works

--- packages/backend/post.ts
+++ packages/backend/post.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express';
 import { ethers } from 'ethers';
 import { LensClient, development } from "@lens-protocol/client";
+import axios from 'axios'; 
+import { v4 as uuidv4 } from 'uuid';
+
 
 require('dotenv').config();
 
@@ -31,41 +34,71 @@ export async function postOnLens(req: Request, res: Response) {
     //lens post
     const profileId = "0x76c7";
 
-    //todo post on arweave/ipfs
-    const contentURI =  "https://zruc54x6ffktxp4gmfvqrfuxtrbws2lvulg7zycohwbhpootj4xa.arweave.net/zGgu8v4pVTu_hmFrCJaXnENpaXWizfzgTj2Cd7nTTy4";    ;
 
-    const collectModule = {
-        simpleCollectModule: {
-          followerOnly: false,
+    // cURL request converted to HTTP POST request using axios
+
+    const body = {
+        "name": req.body.content.slice(0, 20),
+        "content": req.body.content,
+        "metadata_id": `${uuidv4()}`,
+        "version": "1.0.0",
+        "appId": "asap"
+    }
+
+    try {
+        const response = await axios.post('https://api.jsonbin.io/v3/b', {
+            ...body
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+            'X-Master-key': process.env.JSONBIN_PRIVATE_KEY!, // Replace <YOUR_API_KEY> with your actual API key
+            'X-Bin-Private': 'false',
+        },
+        });
+
+        //todo post on arweave/ipfs
+        const contentURI =  `https://api.jsonbin.io/v3/b/${response.data.metadata.id}?meta=false`;
+
+        const collectModule = {
+            simpleCollectModule: {
+            followerOnly: false,
+            }
+        };
+
+        const createPostTypedDataResult = await lensClient.publication.createPostTypedData({
+            profileId,
+            contentURI,
+            collectModule,
         }
-    };
+        );
 
-    const createPostTypedDataResult = await lensClient.publication.createPostTypedData({
-        profileId,
-        contentURI,
-        collectModule,
-      }
-    );
+        const createPostTypedDataValue = createPostTypedDataResult.unwrap();
 
-    const createPostTypedDataValue = createPostTypedDataResult.unwrap();
+        const signedTypedData = await wallet._signTypedData(
+            createPostTypedDataValue.typedData.domain,
+            createPostTypedDataValue.typedData.types,
+            createPostTypedDataValue.typedData.value,
+        );
 
-    const signedTypedData = await wallet._signTypedData(
-        createPostTypedDataValue.typedData.domain,
-        createPostTypedDataValue.typedData.types,
-        createPostTypedDataValue.typedData.value,
-    );
+        console.log(`Broadcasting signed createDataAvailabilityPostTypedData...`);
 
-    console.log(`Broadcasting signed createDataAvailabilityPostTypedData...`);
+        const broadcastResult = await lensClient.transaction.broadcast({
+        id: createPostTypedDataValue.id,
+        signature: signedTypedData,
+        });
 
-    const broadcastResult = await lensClient.transaction.broadcast({
-      id: createPostTypedDataValue.id,
-      signature: signedTypedData,
-    });
+        const broadcastValue = broadcastResult.unwrap();
 
-    const broadcastValue = broadcastResult.unwrap();
+        console.log(`post was created: `, broadcastValue);
 
-    console.log(`post was created: `, broadcastValue);
+        res.status(200).json({ data: "done" })
 
-    res.status(200).json({ data: "done" })
+        console.log('Response from API:', response.data);
+    } catch (error) {
+        console.error('Error making the request:', error);
+        res.status(500).json({ error: "An error occurred while making the request." });
+    }
+
+
   }
 }
