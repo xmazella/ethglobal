d30b30657d6e10efe58ae6f6f03965d9dd45faa2
Guillaume Lodi <guillaume.lodi@gmail.com>
2023-07-22 23:45:01 +0200

    feat: login

--- packages/frontend/src/pages/Login.tsx
+++ packages/frontend/src/pages/Login.tsx
@@ -3,11 +3,10 @@ import { Link } from "react-router-dom"
 import {
   SismoConnectButton,
   SismoConnectConfig,
-  AuthType,
+  // AuthType,
   // ClaimType,
   SismoConnectResponse,
 } from "@sismo-core/sismo-connect-react"
-import useFeed from "../hooks/useFeed"
 
 const config: SismoConnectConfig = {
   appId: "0xafabec94b12842146d5f06acaac25ccd",
@@ -21,7 +20,7 @@ const config: SismoConnectConfig = {
       // "0xA4C94A6091545e40fc9c3E0982AEc8942E282F38",
       // "0x1b9424ed517f7700e7368e34a9743295a225d889",
       // Github Data Source
-      // "github:doliG",
+      // "github:lodig",
       // Twitter Data Source
       // "twitter:dhadrien_",
       // Telegram Data Source
@@ -54,34 +53,24 @@ const Connect = (props: ConnectProps) => (
     <p>Connectez vous avec sismo pour commencer</p>
     <SismoConnectButton
       config={config}
-      auths={[
-        // Anonymous identifier of the vault for this app
-        // vaultId = hash(vaultSecret, appId).
-        // full docs: https://docs.sismo.io/sismo-docs/build-with-sismo-connect/technical-documentation/vault-and-proof-identifiers
-        // user is required to prove ownership of their vaultId for this appId
-        { authType: AuthType.VAULT },
-        // user is required to prove ownership of an EVM account from their vault
-        // { authType: AuthType.EVM_ACCOUNT },
-        // user is required to prove ownership of 0xa4c94a6091545e40fc9c3e0982aec8942e282f38
-        // {
-        //   authType: AuthType.EVM_ACCOUNT,
-        //   userId: "0xa4c94a6091545e40fc9c3e0982aec8942e282f38", // impersonated
-        // },
-        // user is required to prove ownership of a GitHub account
-        { authType: AuthType.GITHUB },
-        // user can prove ownership of a Twitter account, optional
-        { authType: AuthType.TWITTER, isOptional: true },
-        // user can prove ownership of @dhadrien Telegram account, optional
-        //                                   telegram of @dhadrien
-        {
-          authType: AuthType.TELEGRAM,
-          userId: "875608110",
-          isOptional: true,
-        },
+      auths={
+        [
+          // Anonymous identifier of the vault for this app
+          // vaultId = hash(vaultSecret, appId).
+          // full docs: https://docs.sismo.io/sismo-docs/build-with-sismo-connect/technical-documentation/vault-and-proof-identifiers
+          // user is required to prove ownership of their vaultId for this appId
+          // { authType: AuthType.VAULT },
+          // { authType: AuthType.GITHUB },
+          // { authType: AuthType.TWITTER, isOptional: true },
+          // { authType: AuthType.TELEGRAM, userId: "875608110", isOptional: true },
+        ]
+      }
+      claims={[
+        { groupId: "0x75e135ba6f62b00a7ae194920ff8a665" }, // basique
+        { groupId: "0x078b5d9514580634859c634c9f9dff4f", isOptional: true }, // xmtp
+        { groupId: "0xf4777295c7afbdf16e6bd43c93ddcdb5", isOptional: true }, // post on lens
       ]}
-      // we ask the user to sign a message
       signature={{ message: "Connect using sismo", isSelectableByUser: true }}
-      // onResponseBytes calls a 'setResponse' function with the responseBytes returned by the Sismo Vault
       onResponse={props.callback}
     />
   </>
