475559f5345404febe06c7b52b693d8fc1139d4e
Guillaume Lodi <guillaume.lodi@gmail.com>
2023-07-22 14:04:56 +0200

    add: axios and post request to create post

--- packages/frontend/src/components/Tools.ts
+++ packages/frontend/src/components/Tools.ts
@@ -1,33 +1,34 @@
-import { styled } from "styled-components";
-import { MediaSetFragment } from "@lens-protocol/client";
+import { styled } from "styled-components"
+import { MediaSetFragment } from "@lens-protocol/client"
+import axios from "axios"
 
 export const Row = styled.div`
   display: flex;
-`;
+`
 
 export const Column = styled.div`
   display: flex;
   flex-direction: column;
-`;
+`
 
 export const isMediaSetFragment = (
   picture:
     | MediaSetFragment
     | {
-        __typename: "NftImage";
-        contractAddress: string;
-        tokenId: string;
-        uri: string;
-        verified: boolean;
+        __typename: "NftImage"
+        contractAddress: string
+        tokenId: string
+        uri: string
+        verified: boolean
       }
     | null
 ): picture is MediaSetFragment => {
-  return (picture as MediaSetFragment).original !== undefined;
-};
+  return (picture as MediaSetFragment).original !== undefined
+}
 
 export const sanitizeDStorageUrl = (url: string) => {
-  const ipfsGateway = `https://w3s.link/ipfs/`;
-  const arweaveGateway = `arweave.net/`;
+  const ipfsGateway = `https://w3s.link/ipfs/`
+  const arweaveGateway = `arweave.net/`
 
   return url
     .replace(/^Qm[1-9A-Za-z]{44}/gm, `${ipfsGateway}/${url}`)
@@ -35,5 +36,9 @@ export const sanitizeDStorageUrl = (url: string) => {
     .replace("https://ipfs.infura.io/ipfs", ipfsGateway)
     .replace("ipfs://", ipfsGateway)
     .replace("ipfs://ipfs/", ipfsGateway)
-    .replace("ar://", arweaveGateway);
-};
+    .replace("ar://", arweaveGateway)
+}
+
+export function createPost(body: string) {
+  return axios.post("http://localhost:3000/postOnLens", { content: body })
+}
--- packages/frontend/src/pages/Home.tsx
+++ packages/frontend/src/pages/Home.tsx
@@ -1,26 +1,37 @@
-import { styled } from "styled-components";
-import { Column } from "../components/Tools";
-import LensFeed from "../components/LensFeed";
-import Card from "../components/Card";
+import { styled } from "styled-components"
+import { Column, createPost } from "../components/Tools"
+import LensFeed from "../components/LensFeed"
+import Card from "../components/Card"
+import { useState } from "react"
 
 const HomeContainer = styled.div`
   display: flex;
   padding-top: 62px;
   gap: 32px;
-`;
+`
 
 const HomeColumn = styled(Column)`
   align-items: center;
   width: 50%;
   gap: 36px;
-`;
+`
 
 const Home: React.FC<{}> = () => {
+  const [isLoading, setIsLoading] = useState(false)
+
+  const onCreate = async () => {
+    setIsLoading(true)
+    await createPost("// TODO")
+    setIsLoading(false)
+  }
   return (
     <HomeContainer>
       <HomeColumn>
         <Card title="Create a post">
-          <></>
+          <button onClick={onCreate}>
+            Post on lens
+            {isLoading && " ⏳"}
+          </button>
         </Card>
         <Card title="Feed">
           <LensFeed />
@@ -38,7 +49,7 @@ const Home: React.FC<{}> = () => {
         </Card>
       </HomeColumn>
     </HomeContainer>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
